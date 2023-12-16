25f2273f6e897cc87a84691bf9e7f204c833b3bd
Maraxoni <maraxoni16@gmail.com>
2023-12-16 16:35:21 +0100

    first commit

---
+++ Finno-Korean-Hyperwar.cpp
@@ -0,0 +1,22 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+};
+
+bool MyApp::OnInit() {
+    MyFrame* frame = new MyFrame("Hello World", wxPoint(100, 100), wxSize(800, 600));
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size) {}
+
+IMPLEMENT_APP(MyApp)
---
+++ build/CMakeFiles/Finno-Korean-Hyperwar.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Finno-Korean-Hyperwar.
+# This may be replaced when dependencies are built.
